2639c95c0d6b0f1dce3c33e1575db00856838c1e
Neel Mehta <neelmehta@college.harvard.edu>
2016-02-14 22:22:19 -0500

    Load in images

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,13 +1,34 @@
 var React = require('react');
-import { Button } from 'react-bootstrap';
+import { Button, Grid, Row, Col, Image } from 'react-bootstrap';
 
 var Home = React.createClass({
 	render: function() {
+
+		let movies = [
+			{ name: "pfellowship-of-the-ring.png" },
+			{ name: "pfinding-nemo.png" },
+			{ name: "pforrest-gump.png" },
+			{ name: "skypfall.png" },
+			{ name: "the-pforz-awakens.png" }
+		];
+
+		let movieImages = movies.map(movie => {
+			let movieImage = `img/${movie.name}`;
+			return <Col key={movie.name} xs={6} md={3} >
+				<Image src={movieImage} responsive />
+			</Col>;
+		});
+
 		return (
-			<p>
-				Home Page
-				<Button bsStyle="primary">Test</Button>
-			</p>
+			<div>
+				<h4>Popular on Netpflix</h4>
+
+				<Grid>
+					<Row>
+						{movieImages}
+					</Row>
+				</Grid>
+			</div>
 		);
 	}
 });
