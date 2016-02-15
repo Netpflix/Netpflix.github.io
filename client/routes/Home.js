0e45089b0b99d983589c3d69c3eed6b93c7e258c
Neel Mehta <neelmehta@college.harvard.edu>
2016-02-14 23:04:06 -0500

    Replace tabs with spaces

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,36 +1,42 @@
 var React = require('react');
-import { Button, Grid, Row, Col, Image } from 'react-bootstrap';
+import {Button, Grid, Row, Col, Image} from 'react-bootstrap';
 
 var Home = React.createClass({
-	render: function() {
+  render: function() {
 
-		let movies = [
-			{ name: "pfellowship-of-the-ring.png" },
-			{ name: "pfinding-nemo.png" },
-			{ name: "pforrest-gump.png" },
-			{ name: "skypfall.png" },
-			{ name: "the-pforz-awakens.png" }
-		];
+    let movies = [
+      {
+        name: "pfellowship-of-the-ring.png"
+      }, {
+        name: "pfinding-nemo.png"
+      }, {
+        name: "pforrest-gump.png"
+      }, {
+        name: "skypfall.png"
+      }, {
+        name: "the-pforz-awakens.png"
+      }
+    ];
 
-		let movieImages = movies.map(movie => {
-			let movieImage = `img/posters/${movie.name}`;
-			return <Col key={movie.name} xs={6} md={3} >
-				<Image src={movieImage} responsive />
-			</Col>;
-		});
+    let movieImages = movies.map(movie => {
+      let movieImage = `img/posters/${movie.name}`;
+      return <Col key={movie.name} xs={6} md={3}>
+        <Image src={movieImage} responsive/>
+      </Col>;
+    });
 
-		return (
-			<div>
-				<h4>Popular on Netpflix</h4>
+    return (
+      <div>
+        <h4>Popular on Netpflix</h4>
 
-				<Grid>
-					<Row>
-						{movieImages}
-					</Row>
-				</Grid>
-			</div>
-		);
-	}
+        <Grid>
+          <Row>
+            {movieImages}
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
 });
 
 module.exports = Home;
--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,56 +5,56 @@ import Router from 'react-router';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 var Header = React.createClass({
-	render: function() {
-		return (
-			<Navbar inverse>
-		      <Navbar.Header>
-		        <Navbar.Brand>
-		          <a href="/">
-					  <img alt="Netpflix" src="img/netpflix-small.png" className="brand-img" />
-		          </a>
-		        </Navbar.Brand>
-		        <Navbar.Toggle />
-		      </Navbar.Header>
-		      <Navbar.Collapse>
-		        <Nav pullLeft>
-		          <NavItem eventKey={1} href="#" className="active">Browse</NavItem>
-		        </Nav>
-				<Nav pullRight>
-					<NavItem eventKey={2} href="http://pfoho.harvard.edu">Home</NavItem>
-				</Nav>
-		      </Navbar.Collapse>
-		    </Navbar>
-		);
-	}
+  render: function() {
+    return (
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">
+              <img alt="Netpflix" src="img/netpflix-small.png" className="brand-img"/>
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullLeft>
+            <NavItem eventKey={1} href="#" className="active">Browse</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={2} href="http://pfoho.harvard.edu">Home</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 });
 
 var App = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<Header />
-				<div className="container">
-					<Router.RouteHandler/>
-				</div>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div>
+        <Header/>
+        <div className="container">
+          <Router.RouteHandler/>
+        </div>
+      </div>
+    );
+  }
 });
 
 let pages = {
-	Home: require('../routes/Home'),
-	About: require('../routes/About')
+  Home: require('../routes/Home'),
+  About: require('../routes/About')
 };
 
 let routes = (
-	<Router.Route name="app" path="/" handler={App}>
-		<Router.Route name="home" path="/" handler={pages.Home}/>
-		<Router.Route name="about" path="/about" handler={pages.About}/>
-		<Router.DefaultRoute handler={pages.Home}/>
-	</Router.Route>
+  <Router.Route name="app" path="/" handler={App}>
+    <Router.Route name="home" path="/" handler={pages.Home}/>
+    <Router.Route name="about" path="/about" handler={pages.About}/>
+    <Router.DefaultRoute handler={pages.Home}/>
+  </Router.Route>
 );
 
-Router.run(routes, Router.HistoryLocation, function (Handler) {
-	ReactDOM.render(<Handler/>, document.body);
+Router.run(routes, Router.HistoryLocation, function(Handler) {
+  ReactDOM.render(< Handler />, document.body);
 });
