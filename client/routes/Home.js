a3aeef9282034815af85efc9aaf9556c7c37ef49
Neel Mehta <neelmehta@college.harvard.edu>
2016-02-18 21:37:34 -0500

    Add movie descriptions

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -18,27 +18,27 @@ var Home = React.createClass({
         image: "pfellowship-of-the-ring.png"
       }, {
         name: "Pfinding Nemo",
-        description: "",
+        description: "After his son is captured in the Charles River Reef and taken to Mather House, a timid clownfish sets out on a journey to bring him Pfohome.",
         image: "pfinding-nemo.png"
       }, {
         name: "Pforrest Gump",
-        description: "",
+        description: "Pforrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Anne Harrington, eludes him.",
         image: "pforrest-gump.png"
       }, {
         name: "Skypfall",
-        description: "",
+        description: "Bond's loyalty to PF is tested when her past comes back to haunt her. Whilst PFI6 comes under attack, 007 must track down and destroy the threat, no matter how personal the cost.",
         image: "skypfall.png"
       }, {
         name: "Star Wars: The Pforz Awakens",
-        description: "",
+        description: "Three decades after the defeat of the Quadrangle Empire, a new threat arises. The River Order attempts to rule the College and only a ragtag group of heroes can stop them, along with the help of the Pfresistance.",
         image: "the-pforz-awakens.png"
       }, {
         name: "Back to the Pfuture",
-        description: "",
+        description: "A young man is accidentally sent thirty years into the past in a time-traveling Quad Shuttle invented by his friend, Dr. John Durant, and must make sure his Harvard undergrad parents unite in order to save his own existence.",
         image: "back-to-the-pfuture.png"
       }, {
         name: "Pfrozen",
-        description: "",
+        description: "When the newly crowned Queen Elsa accidentally uses her power to turn things into ice to curse the Quad in infinite winter, her sister, Anna, teams up with a Pfoho man, his playful polar bear, and a snowman to change the weather condition.",
         image: "pfrozen.jpg"
       }
     ];
