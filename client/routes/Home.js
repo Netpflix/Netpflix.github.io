ed143ab7e7887848f0b33eaf827d9541c1ff7ef5
Neel Mehta <neelmehta@college.harvard.edu>
2016-02-18 21:25:28 -0500

    Add hover effect

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,31 +1,58 @@
 var React = require('react');
-import {Button, Grid, Row, Col, Image} from 'react-bootstrap';
+import {
+  Button,
+  Grid,
+  Row,
+  Col,
+  Image,
+  Thumbnail
+} from 'react-bootstrap';
 
 var Home = React.createClass({
   render: function() {
 
     let movies = [
       {
-        name: "pfellowship-of-the-ring.png"
+        name: "The Pfellowship of the Ring",
+        description: "A meek Quadling and seven companions set out on a journey to destroy the One Ring and the Dark Lord Adams.",
+        image: "pfellowship-of-the-ring.png"
       }, {
-        name: "pfinding-nemo.png"
+        name: "Pfinding Nemo",
+        description: "",
+        image: "pfinding-nemo.png"
       }, {
-        name: "pforrest-gump.png"
+        name: "Pforrest Gump",
+        description: "",
+        image: "pforrest-gump.png"
       }, {
-        name: "skypfall.png"
+        name: "Skypfall",
+        description: "",
+        image: "skypfall.png"
       }, {
-        name: "the-pforz-awakens.png"
+        name: "Star Wars: The Pforz Awakens",
+        description: "",
+        image: "the-pforz-awakens.png"
       }, {
-        name: "back-to-the-pfuture.png"
+        name: "Back to the Pfuture",
+        description: "",
+        image: "back-to-the-pfuture.png"
       }, {
-        name: "pfrozen.jpg"
+        name: "Pfrozen",
+        description: "",
+        image: "pfrozen.jpg"
       }
     ];
 
     let movieImages = movies.map(movie => {
-      let movieImage = `img/posters/${movie.name}`;
+      let movieImage = `img/posters/${movie.image}`;
       return <Col key={movie.name} xs={6} md={3}>
-        <Image src={movieImage} responsive/>
+
+        <Thumbnail src={movieImage} alt={movie.name} className="movie-poster">
+          <div className="movie-details">
+            <h4>{movie.name}</h4>
+            <p>{movie.description}</p>
+          </div>
+        </Thumbnail>
       </Col>;
     });
 
