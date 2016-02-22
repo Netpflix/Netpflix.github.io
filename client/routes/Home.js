b8d43e8f591a8c7d5d7bbe000aa9377743c9151d
Neel Mehta <neelmehta@college.harvard.edu>
2016-02-22 15:30:38 -0500

    Make navbar sticky

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -44,54 +44,56 @@ var Home = React.createClass({
 
     let movies = [
       {
+        name: "Star Wars: The Pforz Awakens",
+        description: `Three decades after the defeat of the Quadrangle Empire, a new threat arises. The
+                  River Order attempts to rule the College and only a ragtag group of heroes can stop them,
+                  along with the help of the Pfresistance.`,
+        category: categories.NEW_ARRIVAL,
+        image: "the-pforz-awakens.png"
+      }, {
         name: "The Pfellowship of the Ring",
         description: `A meek Quadling and seven blockmates set out on a journey to destroy the
-            One Ring and the Dark Lord Adams.`,
+                One Ring and the Dark Lord Adams.`,
         category: categories.POPULAR,
         image: "pfellowship-of-the-ring.png"
       }, {
         name: "Pfinding Nemo",
         description: `After his son is captured in the Charles River Reef and taken to Mather House, a
-            timid clownfish sets out on a journey to bring him Pfohome.`,
+                timid clownfish sets out on a journey to bring him Pfohome.`,
         category: categories.POPULAR,
         image: "pfinding-nemo.png"
       }, {
-        name: "Pforrest Gump",
-        description: `Pforrest Gump, while not intelligent, has accidentally been present at many
-            historic moments, but his true love, Pfoho, eludes him.`,
-        category: categories.POPULAR,
-        image: "pforrest-gump.png"
-      }, {
-        name: "Skypfall",
-        description: `Bond's loyalty to PF is tested when her past comes back to haunt her. Whilst
-            PfI6 comes under attack, 007 must track down and destroy the threat, no matter how
-            personal the cost.`,
-        category: categories.POPULAR,
-        image: "skypfall.png"
-      }, {
-        name: "Star Wars: The Pforz Awakens",
-        description: `Three decades after the defeat of the Quadrangle Empire, a new threat arises. The
-            River Order attempts to rule the College and only a ragtag group of heroes can stop them,
-            along with the help of the Pfresistance.`,
-        category: categories.NEW_ARRIVAL,
-        image: "the-pforz-awakens.png"
-      }, {
         name: "Back to the Pfuture",
         description: `A young man is accidentally sent thirty years into the past in a time-traveling
-            Quad Shuttle invented by his friend, Dr. Carl Pforzheimer, and must make sure his
-            college-age parents unite in order to save his own existence.`,
+                Quad Shuttle invented by his friend, Dr. Carl Pforzheimer, and must make sure his
+                college-age parents unite in order to save his own existence.`,
         category: categories.POPULAR,
         image: "back-to-the-pfuture.png"
       }, {
+        name: "Skypfall",
+        description: `Bond's loyalty to PF is tested when her past comes back to haunt her. Whilst
+                PfI6 comes under attack, 007 must track down and destroy the threat, no matter how
+                personal the cost.`,
+        category: categories.POPULAR,
+        image: "skypfall.png"
+      }, {
         name: "Pfrozen",
         description: `When the newly crowned Queen Elsa accidentally uses her power to turn things into
-            ice to curse the Quad in infinite winter, her sister, Anna, teams up with a Pfoho man,
-            his playful polar bear, and a snowman to change the weather condition.`,
+                ice to curse the Quad in infinite winter, her sister, Anna, teams up with a Pfoho man,
+                his playful polar bear, and a snowman to change the weather condition.`,
         category: categories.RECENT,
         image: "pfrozen.jpg",
         video: "https://www.youtube.com/watch?v=uKPrkR1wkfA"
       }
     ];
+
+    // {
+    //   name: "Pforrest Gump",
+    //   description: `Pforrest Gump, while not intelligent, has accidentally been present at many
+    //       historic moments, but his true love, Pfoho, eludes him.`,
+    //   category: categories.POPULAR,
+    //   image: "pforrest-gump.png"
+    // },
 
     /**
      * Given a list of movies and a category, reutrns posters of movies in
