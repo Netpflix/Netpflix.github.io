55c99c3423909cf6947f7827c9a27c61e584dd8b
Neel Mehta <neelmehta96@gmail.com>
2018-03-04 23:13:52 -0500

    css adjustments"
    "

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-$(function(){
-  // $(".card-img").on("mouseover", function(){
-  //   $(this).parent().find(".movie-overlay").css("display","block");
-  // });
-  $(".movie-overlay").on("mouseover mouseout", function(){
-    $(this).toggleClass("transparent");
-  });
-});
+// $(function(){
+//   // $(".card-img").on("mouseover", function(){
+//   //   $(this).parent().find(".movie-overlay").css("display","block");
+//   // });
+//   $(".movie-overlay").on("mouseover mouseout", function(){
+//     $(this).toggleClass("transparent");
+//   });
+// });
