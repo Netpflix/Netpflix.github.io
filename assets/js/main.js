4a98328f60d7c5b72da24e8c2b6da179e4e2a136
Neel Mehta <neelmehta96@gmail.com>
2018-03-07 18:04:06 -0500

    make it work on click and hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@
 //     $(this).toggleClass("transparent");
 //   });
 // });
+
+$(function(){
+  $(".movie-overlay").on("click mouseover", function(){
+    $(this).css("opacity", 1);
+
+    // turn off all other movie-overlays' opacity
+    $(".movie-overlay").not(this).css("opacity", 0);
+  });
+});
